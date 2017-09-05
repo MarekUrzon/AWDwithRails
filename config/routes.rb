aa320273c64caff10554b0029f88de263f4ceb41
MarekUzi <marekurzon@gmail.com>
2017-09-05 22:00:37 +0200

    Add a Dash of Ajax

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  resources :carts
-  resources :line_items
-  resources :cards
+	resources :orders
+	resources :carts
+	resources :line_items
+	resources :cards
 	root 'store#index',as: 'store/index'
 
-	resources :products
+	resources :products do
+		get :who_bought, on: :member
+	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
