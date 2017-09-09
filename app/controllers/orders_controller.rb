55346178adecb11d0892597c0a9e1badba567286
MarekUzi <marekurzon@gmail.com>
2017-09-09 15:50:56 +0200

    AJ Slow Payment Processor

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -34,15 +34,18 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
-        OrderMailer.received(@order).deliver_later
-        format.html { redirect_to store_index_url, notice: 'Thank you for your order.' }
-        format.json { render :show, status: :created, location: @order }
-      else
-        format.html { render :new }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+        ChargeOrderJob.perform_later(@order,pay_type_params.to_h)
+        format.html { redirect_to store_index_url, notice: 
+          'Thank you for your order.' }
+          format.json { render :show, status: :created,
+            location: @order }
+          else
+            format.html { render :new }
+            format.json { render json: @order.errors,
+              status: :unprocessable_entity }
+            end
+          end
+        end
 
   # PATCH/PUT /orders/1
   # PATCH/PUT /orders/1.json
@@ -69,11 +72,6 @@ class OrdersController < ApplicationController
   end
 
   private
-  def ensure_cart_isnt_empty
-    if @cart.line_items.empty?
-      redirect_to store_index_url, notice: 'Your cart is empty'
-    end
-  end
     # Use callbacks to share common setup or constraints between actions.
     def set_order
       @order = Order.find(params[:id])
@@ -83,17 +81,25 @@ class OrdersController < ApplicationController
     def order_params
       params.require(:order).permit(:name, :address, :email, :pay_type)
     end
+  #...
 
-    def pay_type_params
-      if order_params[:pay_type] == "Credit Card"
-        params.require(:order).permit(:credit_card_number, :expiration_date)
-      elsif order_params[:pay_type] == "Check"
-        params.require(:order).permit(:routing_number, :account_number)
-      elsif order_params[:pay_type] == "Purchase Order"
-        params.require(:order).permit(:po_number)
-      else
-        {}
-      end
-    end
-    
+  private
+  def ensure_cart_isnt_empty
+   if @cart.line_items.empty?
+     redirect_to store_index_url, notice: 'Your cart is empty'
+   end
+ end
+
+ def pay_type_params  
+  if order_params[:pay_type] == "Credit card"
+    params.require(:order).permit(:credit_card_number, :expiration_date)
+  elsif order_params[:pay_type] == "Check"
+    params.require(:order).permit(:routing_number, :account_number)
+  elsif order_params[:pay_type] == "Purchase Order"
+    params.require(:order).permit(:po_number)
+  else
+    {}
   end
+end
+
+end
--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -1,7 +1,11 @@
 require "application_system_test_case"
 
 class OrdersTest < ApplicationSystemTestCase
+	include ActiveJob::TestHelper
 	test "check routing number" do 
+		LineItem.delete_all
+		Order.delete_all
+
 		visit store_index_url
 
 		first('.entry').click_on 'Add to Cart'
@@ -14,16 +18,32 @@ class OrdersTest < ApplicationSystemTestCase
 
 		assert_no_selector "#order_routing_number"
 
-		select 'Check', from: 'pay_type'
-		assert_selector "#order_routing_number"
-		assert_selector "#order_account_number"
-
 		select 'Credit card', from: 'pay_type'
 		assert_selector "#order_credit_card_number"
 		assert_selector "#order_expiration_date"
 
 		select 'Purchase order', from: 'pay_type'
 		assert_selector "#order_po_number"
+
+		select 'Check', from: 'pay_type'
+		assert_selector "#order_routing_number"
+		fill_in "Routing #", with: "123456"
+		fill_in "Account #", with: "987654"
+		assert_selector "#order_account_number"
+
+		perform_enqueued_jobs do 
+			click_button "Place Order" 
+		end
+
+		orders = Order.all
+		assert_equal 1, orders.size
+		order = orders.first
+		assert_equal "Dave Thomas", order.name
+		assert_equal "123 Main Street", order.address
+		assert_equal "dave@example.com", order.email
+		assert_equal "Check", order.pay_type
+		assert_equal 1, order.line_items.size
+
 	end
 
 	test "Add to Cart should invoke Your Cart" do
